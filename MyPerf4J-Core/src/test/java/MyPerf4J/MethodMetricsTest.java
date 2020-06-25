7f3e159be07d2fc4d7063be6b73c40d24a05148a
LinShunKang <linshunkang.chn@gmail.com>
2020-06-25 18:38:00 +0800

    fix: Fix the problem of TP calculation error under extremely uneven distribution

--- MyPerf4J-Core/src/test/java/MyPerf4J/MethodMetricsTest.java
+++ MyPerf4J-Core/src/test/java/MyPerf4J/MethodMetricsTest.java
@@ -6,8 +6,8 @@ import cn.myperf4j.core.*;
 import cn.myperf4j.core.recorder.AccurateRecorder;
 import cn.myperf4j.core.recorder.Recorder;
 import cn.myperf4j.core.recorder.Recorders;
-import cn.myperf4j.core.recorder.RoughRecorder;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.concurrent.atomic.AtomicReferenceArray;
@@ -17,56 +17,130 @@ import java.util.concurrent.atomic.AtomicReferenceArray;
  */
 public class MethodMetricsTest {
 
-    @Test
-    public void test() {
-        Recorders recorders = new Recorders(new AtomicReferenceArray<Recorder>(10));
-        MethodTagMaintainer methodTagMaintainer = MethodTagMaintainer.getInstance();
+    private final MethodTagMaintainer methodTagMaintainer = MethodTagMaintainer.getInstance();
 
-        int methodId1 = methodTagMaintainer.addMethodTag(MethodTag.getGeneralInstance("", "Test", "Api", "m1", ""));
-        recorders.setRecorder(methodId1, AccurateRecorder.getInstance(0, 9000, 50));
-        testNormalRecord(recorders, methodTagMaintainer, methodId1);
+    private final Recorders recorders = new Recorders(new AtomicReferenceArray<Recorder>(10));
 
-        int methodId2 = methodTagMaintainer.addMethodTag(MethodTag.getGeneralInstance("", "Test", "Api", "m2", ""));
-        recorders.setRecorder(methodId2, RoughRecorder.getInstance(0, 10000));
-        testNormalRecord(recorders, methodTagMaintainer, methodId2);
+    private Recorder recorder;
 
-        int methodId3 = methodTagMaintainer.addMethodTag(MethodTag.getGeneralInstance("", "Test", "Api", "m3", ""));
-        recorders.setRecorder(methodId3, RoughRecorder.getInstance(0, 10000));
-        testZeroRecor(recorders, methodTagMaintainer, methodId3);
+    @Before
+    public void init() {
+        MethodTag methodTag = MethodTag.getGeneralInstance("", "Test", "Api", "m1", "");
+        int methodTagId = methodTagMaintainer.addMethodTag(methodTag);
+        recorder = AccurateRecorder.getInstance(methodTagId, 1000, 50);
     }
 
-    private void testNormalRecord(Recorders recorders, MethodTagMaintainer methodTagMaintainer, int methodId) {
-        Recorder recorder = recorders.getRecorder(methodId);
-        recorders.setStartTime(System.currentTimeMillis());
+    @Test
+    public void testUniformDistribution() {
+        Recorder recorder = AccurateRecorder.getInstance(1, 1024, 512);
         long start = System.nanoTime();
-        for (long i = 0; i < 10000; ++i) {
+        for (long i = 1; i <= 10000; ++i) {
             recorder.recordTime(start, start + i * 1000 * 1000);
         }
-        recorders.setStopTime(System.currentTimeMillis());
 
-        MethodTag methodTag = methodTagMaintainer.getMethodTag(methodId);
-        MethodMetrics methodMetrics = MethodMetricsCalculator.calPerfStats(recorder, methodTag, recorders.getStartTime(), recorders.getStopTime());
+        long startMillis = System.currentTimeMillis();
+        MethodTag methodTag = methodTagMaintainer.getMethodTag(recorder.getMethodTagId());
+        MethodMetrics methodMetrics = MethodMetricsCalculator.calPerfStats(recorder, methodTag, startMillis, startMillis + 1000);
         System.out.println(methodMetrics);
-        recorder.resetRecord();
 
-        Assert.assertEquals(methodMetrics.getMinTime(), 0);
-        assert methodMetrics.getAvgTime() == 4999.5D;
-        Assert.assertEquals(methodMetrics.getTP50(), 4999);
-        Assert.assertEquals(methodMetrics.getTP90(), 8999);
-        Assert.assertEquals(methodMetrics.getTP95(), 9499);
-        Assert.assertEquals(methodMetrics.getTP99(), 9899);
-        Assert.assertEquals(methodMetrics.getTP999(), 9989);
-        Assert.assertEquals(methodMetrics.getTP9999(), 9998);
-        Assert.assertEquals(methodMetrics.getTP100(), 9999);
+        Assert.assertEquals(methodMetrics.getMinTime(), 1);
+        assert methodMetrics.getAvgTime() == 5000.5D;
+        Assert.assertEquals(methodMetrics.getTP50(), 5000);
+        Assert.assertEquals(methodMetrics.getTP90(), 9000);
+        Assert.assertEquals(methodMetrics.getTP95(), 9500);
+        Assert.assertEquals(methodMetrics.getTP99(), 9900);
+        Assert.assertEquals(methodMetrics.getTP999(), 9990);
+        Assert.assertEquals(methodMetrics.getTP9999(), 9999);
+        Assert.assertEquals(methodMetrics.getTP100(), 10000);
         Assert.assertEquals(methodMetrics.getTP100(), methodMetrics.getMaxTime());
     }
 
-    private void testZeroRecor(Recorders recorders, MethodTagMaintainer methodTagMaintainer, int methodId) {
-        Recorder recorder = recorders.getRecorder(methodId);
+    @Test
+    public void testExtremelyUnevenDistribution() {
+        recordRecords(recorder, 1L, 10000);
+
+        long startMillis = System.currentTimeMillis();
+        MethodTag methodTag = methodTagMaintainer.getMethodTag(recorder.getMethodTagId());
+        MethodMetrics methodMetrics = MethodMetricsCalculator.calPerfStats(recorder, methodTag, startMillis, startMillis + 1000);
+        System.out.println(methodMetrics);
+
+        Assert.assertEquals(methodMetrics.getMinTime(), 1);
+        assert methodMetrics.getAvgTime() == 1D;
+        Assert.assertEquals(methodMetrics.getTP50(), 1);
+        Assert.assertEquals(methodMetrics.getTP90(), 1);
+        Assert.assertEquals(methodMetrics.getTP95(), 1);
+        Assert.assertEquals(methodMetrics.getTP99(), 1);
+        Assert.assertEquals(methodMetrics.getTP999(), 1);
+        Assert.assertEquals(methodMetrics.getTP9999(), 1);
+        Assert.assertEquals(methodMetrics.getTP100(), 1);
+        Assert.assertEquals(methodMetrics.getTP100(), methodMetrics.getMaxTime());
+    }
+
+    private void recordRecords(Recorder recorder, long elapsedMills, int times) {
+        long start = System.nanoTime();
+        for (long i = 0; i < times; ++i) {
+            recorder.recordTime(start, start + elapsedMills * 1000 * 1000L);
+        }
+    }
+
+    @Test
+    public void testExtremelyUnevenDistribution2() {
+        recordRecords(recorder, 1L, 4000);
+        recordRecords(recorder, 2L, 3000);
+        recordRecords(recorder, 3L, 2000);
+        recordRecords(recorder, 4L, 500);
+        recordRecords(recorder, 5L, 300);
+        recordRecords(recorder, 6L, 200);
+
+        long startMillis = System.currentTimeMillis();
+        MethodTag methodTag = methodTagMaintainer.getMethodTag(recorder.getMethodTagId());
+        MethodMetrics methodMetrics = MethodMetricsCalculator.calPerfStats(recorder, methodTag, startMillis, startMillis + 1000);
+        System.out.println(methodMetrics);
+
+        Assert.assertEquals(methodMetrics.getMinTime(), 1);
+        assert methodMetrics.getAvgTime() == 2.07D;
+        Assert.assertEquals(methodMetrics.getTP50(), 2);
+        Assert.assertEquals(methodMetrics.getTP90(), 3);
+        Assert.assertEquals(methodMetrics.getTP95(), 4);
+        Assert.assertEquals(methodMetrics.getTP99(), 6);
+        Assert.assertEquals(methodMetrics.getTP999(), 6);
+        Assert.assertEquals(methodMetrics.getTP9999(), 6);
+        Assert.assertEquals(methodMetrics.getTP100(), 6);
+        Assert.assertEquals(methodMetrics.getTP100(), methodMetrics.getMaxTime());
+    }
+
+    @Test
+    public void testExtremelyUnevenDistribution3() {
+        recordRecords(recorder, 1L, 4000);
+        recordRecords(recorder, 2L, 5600);
+        recordRecords(recorder, 3L, 300);
+        recordRecords(recorder, 4L, 90);
+        recordRecords(recorder, 5L, 9);
+        recordRecords(recorder, 6L, 1);
+
+        long startMillis = System.currentTimeMillis();
+        MethodTag methodTag = methodTagMaintainer.getMethodTag(recorder.getMethodTagId());
+        MethodMetrics methodMetrics = MethodMetricsCalculator.calPerfStats(recorder, methodTag, startMillis, startMillis + 1000);
+        System.out.println(methodMetrics);
+
+        Assert.assertEquals(methodMetrics.getMinTime(), 1);
+        assert methodMetrics.getAvgTime() == 1.6511D;
+        Assert.assertEquals(methodMetrics.getTP50(), 2);
+        Assert.assertEquals(methodMetrics.getTP90(), 2);
+        Assert.assertEquals(methodMetrics.getTP95(), 2);
+        Assert.assertEquals(methodMetrics.getTP99(), 3);
+        Assert.assertEquals(methodMetrics.getTP999(), 4);
+        Assert.assertEquals(methodMetrics.getTP9999(), 5);
+        Assert.assertEquals(methodMetrics.getTP100(), 6);
+        Assert.assertEquals(methodMetrics.getTP100(), methodMetrics.getMaxTime());
+    }
+
+    @Test
+    public void testZeroRecord() {
         recorders.setStartTime(System.currentTimeMillis());
         recorders.setStopTime(System.currentTimeMillis() + 1000);
 
-        MethodTag methodTag = methodTagMaintainer.getMethodTag(methodId);
+        MethodTag methodTag = methodTagMaintainer.getMethodTag(recorder.getMethodTagId());
         MethodMetrics methodMetrics = MethodMetricsCalculator.calPerfStats(recorder, methodTag, recorders.getStartTime(), recorders.getStopTime());
         System.out.println(methodMetrics);
         recorder.resetRecord();
@@ -82,4 +156,5 @@ public class MethodMetricsTest {
         Assert.assertEquals(methodMetrics.getTP100(), -1);
         Assert.assertEquals(methodMetrics.getTP100(), methodMetrics.getMaxTime());
     }
+
 }
