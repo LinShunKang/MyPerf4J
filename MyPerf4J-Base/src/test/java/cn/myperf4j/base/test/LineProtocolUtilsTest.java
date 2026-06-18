16606ae504f62e940e7a4c5674feacacc0916241
LinShunKang <linshunkang.chn@gmail.com>
2026-06-18 22:34:36 +0800

    Perf: Simple optimize LineProtocolUtils

--- MyPerf4J-Base/src/test/java/cn/myperf4j/base/test/LineProtocolUtilsTest.java
+++ MyPerf4J-Base/src/test/java/cn/myperf4j/base/test/LineProtocolUtilsTest.java
@@ -12,12 +12,25 @@ public class LineProtocolUtilsTest {
 
     @Test
     public void test() {
-        String expect = "method_metrics\\,AppName\\=TestApp\\,ClassName\\=TestClass\\,Method\\=TestClass.test\\ " +
+        final String str = "method_metrics\\,AppName\\=TestApp\\,ClassName\\=TestClass\\,Method\\=TestClass.test\\ " +
                 "RPS\\=1i\\,Avg\\=0.00\\,Min\\=0i\\,Max\\=0i\\,StdDev\\=0.00\\,Count\\=17i\\,TP50\\=0i\\,TP90\\=0i\\," +
                 "TP95\\=0i\\,TP99\\=0i\\,TP999\\=0i\\,TP9999\\=0i\\,TP99999\\=0i\\,TP100\\=0i\\ 1539705590006000000";
-        Assert.assertEquals(expect,
+        Assert.assertEquals(str,
                 processTagOrField("method_metrics,AppName=TestApp,ClassName=TestClass,Method=TestClass.test " +
                         "RPS=1i,Avg=0.00,Min=0i,Max=0i,StdDev=0.00,Count=17i,TP50=0i,TP90=0i," +
                         "TP95=0i,TP99=0i,TP999=0i,TP9999=0i,TP99999=0i,TP100=0i 1539705590006000000"));
+    }
+
+    @Test
+    public void testEscapeSpecialChars() {
+        Assert.assertEquals("a\\ b\\,c\\=d", processTagOrField("a b,c=d"));
+        Assert.assertEquals("\\,\\ \\=", processTagOrField(", ="));
+    }
+
+    @Test
+    public void testNoEscapeChars() {
+        final String str = "abcdefg";
+        Assert.assertEquals(str, processTagOrField(str));
+        Assert.assertEquals(str, processTagOrField(str));
     }
 }
